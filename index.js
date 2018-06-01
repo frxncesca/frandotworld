ebdc0b9b64cc4a900d5f6435280bfb5a2ac40b4f
Khalil Stemmler <metroidman12@gmail.com>
2018-06-01 01:59:17 -0400

    Initial commit.

---
+++ index.js
@@ -0,0 +1,16 @@
+
+/**
+ * main
+ * 
+ * Invoked when the page is loaded.
+ * All custom JS code can go within this function.
+ */
+
+function main () {
+
+  console.log("Welcome to Fran world!");
+
+}
+
+// Load
+$(document).ready(main)
