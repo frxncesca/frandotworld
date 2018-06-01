539970e273d7cb955401ce9cd49e75e524c79b2e
Khalil Stemmler <metroidman12@gmail.com>
2018-06-01 03:03:41 -0400

    Added gifs on hover on desktop version.

--- index.js
+++ index.js
@@ -10,6 +10,26 @@ function main () {
 
   console.log("Welcome to Fran world!");
 
+  /**
+   * onHoverCaseStudy
+   * 
+   * When we hover over a case study, it will
+   * switch to animating that GIF.
+   */
+
+  function onHoverCaseStudy () {
+    let caseStudyNum = $(this).attr('data-cs');
+    $(this).css("background-image", `url(assets/gifs/gif${caseStudyNum}.gif)`);
+  }
+
+  function onLeaveHoverCaseStudy () {
+    let caseStudyNum = $(this).attr('data-cs');
+    $(this).css("background-image", `url(assets/static/static${caseStudyNum}.PNG)`);
+  }
+
+
+  $(".case-study-wrapper").hover(onHoverCaseStudy, onLeaveHoverCaseStudy); 
+
 }
 
 // Load
